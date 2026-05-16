25b3feb2c4e6f808e023bdb1d2a6e854262f3ea3
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-05-16 22:57:01 +0100

    app and middlewares

--- middleware-costum/middleware/usernameMiddleware.js
+++ middleware-costum/middleware/usernameMiddleware.js
@@ -1,4 +1,4 @@
-function usernameMiddleware(req, res, next) {
+export default function usernameMiddleware(req, res, next) {
   const username = req.header("X-Username");
 
   req.username = username || null;
