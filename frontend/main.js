cd9ac09550abda43e919a8c30ab85f12b858d6ab
Fanglai <laura@FLMacBook-Air.local>
2026-05-23 10:36:03 +0200

    fixed link issue

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,13 @@
-const socket = io();
+const socket = io(
+  "http://api.ylge3zmlikef3zm52i14oo8o.178.105.39.91.sslip.io"
+);
 
 const chatBox = document.getElementById("chat-box");
-
 const sendBtn = document.getElementById("sendBtn");
+const messageInput = document.getElementById("message");
+const usernameInput = document.getElementById("username");
 
-const messageInput =
-document.getElementById("message");
-
-const usernameInput =
-document.getElementById("username");
-
-function addMessage(msg){
+function addMessage(msg) {
 
     const div = document.createElement("div");
 
@@ -19,51 +16,49 @@ function addMessage(msg){
     div.innerHTML = `
         <strong>${msg.username}</strong>
         (${msg.time})
-
         <br>
-
         ${msg.text}
     `;
 
     chatBox.appendChild(div);
-
 }
 
-socket.on("loadMessages",(messages)=>{
+socket.on("connect", () => {
+    console.log("Connected:", socket.id);
+});
 
-    chatBox.innerHTML="";
+socket.on("loadMessages", (messages) => {
+
+    chatBox.innerHTML = "";
 
     messages.forEach(addMessage);
 
 });
 
-socket.on("newMessage",(msg)=>{
+socket.on("newMessage", (msg) => {
 
     addMessage(msg);
 
 });
 
-sendBtn.addEventListener("click",()=>{
+sendBtn.addEventListener("click", () => {
 
-    const text = messageInput.value;
+    const text = messageInput.value.trim();
 
     const username =
-        usernameInput.value || "Anonymous";
+        usernameInput.value.trim() || "Anonymous";
 
-    if(!text) return;
+    if (!text) return;
 
     const msg = {
 
         username,
-
         text,
-
-        time:new Date()
-            .toLocaleTimeString()
+        time: new Date().toLocaleTimeString()
 
     };
 
     socket.emit("sendMessage", msg);
 
-    messageInput.value="";
+    messageInput.value = "";
 });
